0d7504eb696a88216f3a0f630e36aa7d37ed4e82
Anibal Olivares <olivaresluisanibal@gmail.com>
2021-01-13 20:00:54 +0000

    Fixing URL key

--- components/MapContainer.tsx
+++ components/MapContainer.tsx
@@ -5,7 +5,7 @@ import Mark from "./icons/Mark";
 const Map: React.FC = (): JSX.Element => {
   return (
     <GoogleMapReact
-      bootstrapURLKeys={{ key: "AIzaSyBmcql6sxiES3IDXOcvbRieCuqg4suFqxs" }}
+      bootstrapURLKeys={{ key: "" }}
       defaultCenter={{ lat: 59.95, lng: 30.33 }}
       defaultZoom={11}>
       <Mark lat={59.955413} lng={30.337844} />
