e872f9fa2067a92e80d239115a65fa2032282cfa
Anibal Olivares <olivaresluisanibal@gmail.com>
2021-01-15 16:20:12 +0000

    [UPDATE] IPContainer CSS Fix

--- components/IPContainer.tsx
+++ components/IPContainer.tsx
@@ -12,13 +12,13 @@ type Props = {
 const IPContainer: React.FC<Props> = ({ data, className, style }): JSX.Element => {
   return (
     <ul
-      className={`w-9/12 bg-white shadow-lg rounded-lg flex flex-wrap overflow-hidden divide-x divide-gray-800 divide-opacity-25 ${className}`}
+      className={`md:w-9/12 w-2/5  bg-white shadow-lg rounded-lg flex flex-wrap overflow-hidden divide-x divide-gray-800 divide-opacity-25 ${className}`}
       style={style}>
       <li className="w-full overflow-hidden sm:w-full md:w-1/4 p-4">
         <IPDetails section="IP ADDRESS" data={data.ip} />
       </li>
       <li className="w-full overflow-hidden sm:w-full md:w-1/4 p-4">
-        <IPDetails section="LOCATION" data={`${data.country}/${data.city}`} />
+        <IPDetails section="LOCATION" data={`${data.country} / ${data.city}`} />
       </li>
       <li className="w-full overflow-hidden sm:w-full md:w-1/4 p-4">
         <IPDetails section="TIMEZONE" data={data.timezone} />
